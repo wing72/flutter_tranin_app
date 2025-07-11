d755c27fe77562af5183364827074f613392e37f
wing72 <t2024-m0185@t2024-m0185ui-MacBookAir.local>
2025-07-11 22:37:12 +0900

    feat button push appbar Title change

--- lib/page/station/stationlistpage.dart
+++ lib/page/station/stationlistpage.dart
@@ -2,24 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter_train_app/page/home/homepage.dart';
 
 class Stationlistpage extends StatelessWidget {
-  const Stationlistpage({super.key});
+  final String addTitle;
+
+  const Stationlistpage({
+    super.key,
+    required this.addTitle,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
           centerTitle: true,
-          title: const Text('출발역'),
+          title: Text(addTitle),
           leading: const BackButton()),
       body: Column(
         children: [
           Expanded(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 ElevatedButton(
                   style: TextButton.styleFrom(
-                    minimumSize: Size(50, 30),
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    shadowColor: Colors.transparent,
+                    minimumSize: Size(30, 30),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20),
                     ),
@@ -27,10 +36,169 @@ class Stationlistpage extends StatelessWidget {
                   onPressed: () {
                     Navigator.pop(context, '수서');
                   },
-                  child: const Text('수서역 선택'),
+                  child:
+                      const Text('수서', style: TextStyle(color: Colors.black)),
                 ),
-                const SizedBox(height: 10),
-                SizedBox(height: 8),
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    shadowColor: Colors.transparent,
+                    minimumSize: Size(30, 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, '동탄');
+                  },
+                  child:
+                      const Text('동탄', style: TextStyle(color: Colors.black)),
+                ),
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    shadowColor: Colors.transparent,
+                    minimumSize: Size(30, 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, '평택지제');
+                  },
+                  child:
+                      const Text('평택지제', style: TextStyle(color: Colors.black)),
+                ),
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    shadowColor: Colors.transparent,
+                    minimumSize: Size(30, 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, '천안아산');
+                  },
+                  child:
+                      const Text('천안아산', style: TextStyle(color: Colors.black)),
+                ),
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    shadowColor: Colors.transparent,
+                    minimumSize: Size(30, 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, '오송');
+                  },
+                  child:
+                      const Text('오송', style: TextStyle(color: Colors.black)),
+                ),
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    shadowColor: Colors.transparent,
+                    minimumSize: Size(30, 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, '대전');
+                  },
+                  child:
+                      const Text('대전', style: TextStyle(color: Colors.black)),
+                ),
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    shadowColor: Colors.transparent,
+                    minimumSize: Size(30, 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, '김천구미');
+                  },
+                  child:
+                      const Text('김천구미', style: TextStyle(color: Colors.black)),
+                ),
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    shadowColor: Colors.transparent,
+                    minimumSize: Size(30, 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, '동대구');
+                  },
+                  child:
+                      const Text('동대구', style: TextStyle(color: Colors.black)),
+                ),
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    shadowColor: Colors.transparent,
+                    minimumSize: Size(30, 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, '경주');
+                  },
+                  child:
+                      const Text('경주', style: TextStyle(color: Colors.black)),
+                ),
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    shadowColor: Colors.transparent,
+                    minimumSize: Size(30, 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, '울산');
+                  },
+                  child:
+                      const Text('울산', style: TextStyle(color: Colors.black)),
+                ),
+                ElevatedButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    shadowColor: Colors.transparent,
+                    minimumSize: Size(30, 30),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, '부산');
+                  },
+                  child:
+                      const Text('부산', style: TextStyle(color: Colors.black)),
+                ),
               ],
             ),
           ),
